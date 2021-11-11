e7f3db7f4dfe87238e75a4756c02104ca6e758b9
Warparri 312 <Kralman895@hotmail.com>
2021-11-11 21:34:07 +0300

    bg change#1

--- d20/Form1.Designer.cs
+++ d20/Form1.Designer.cs
@@ -52,29 +52,32 @@ namespace d20
             // 
             // rollbutton
             // 
+            this.rollbutton.BackColor = System.Drawing.Color.Transparent;
             this.rollbutton.FlatAppearance.BorderColor = System.Drawing.Color.LightCyan;
             this.rollbutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
             this.rollbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
             this.rollbutton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.rollbutton.Font = new System.Drawing.Font("Unispace", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rollbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.rollbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(93)))), ((int)(((byte)(46)))));
-            this.rollbutton.Location = new System.Drawing.Point(138, 320);
+            this.rollbutton.Location = new System.Drawing.Point(184, 394);
+            this.rollbutton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rollbutton.Name = "rollbutton";
-            this.rollbutton.Size = new System.Drawing.Size(180, 65);
+            this.rollbutton.Size = new System.Drawing.Size(240, 80);
             this.rollbutton.TabIndex = 1;
             this.rollbutton.Text = "ROLL";
-            this.rollbutton.UseVisualStyleBackColor = true;
+            this.rollbutton.UseVisualStyleBackColor = false;
             this.rollbutton.Click += new System.EventHandler(this.button1_Click);
             // 
             // lblChangeText
             // 
             this.lblChangeText.AutoSize = true;
             this.lblChangeText.BackColor = System.Drawing.Color.Transparent;
-            this.lblChangeText.Font = new System.Drawing.Font("Unispace", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblChangeText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblChangeText.ForeColor = System.Drawing.Color.Black;
-            this.lblChangeText.Location = new System.Drawing.Point(235, 118);
+            this.lblChangeText.Location = new System.Drawing.Point(313, 145);
+            this.lblChangeText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblChangeText.Name = "lblChangeText";
-            this.lblChangeText.Size = new System.Drawing.Size(0, 23);
+            this.lblChangeText.Size = new System.Drawing.Size(0, 29);
             this.lblChangeText.TabIndex = 3;
             this.lblChangeText.Tag = "";
             this.lblChangeText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -82,35 +85,41 @@ namespace d20
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Unispace", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(93)))), ((int)(((byte)(46)))));
-            this.button1.Location = new System.Drawing.Point(325, 240);
+            this.button1.Location = new System.Drawing.Point(433, 295);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(39, 145);
+            this.button1.Size = new System.Drawing.Size(52, 178);
             this.button1.TabIndex = 4;
             this.button1.Text = "X";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // bruh1
             // 
             this.bruh1.AutoSize = true;
+            this.bruh1.BackColor = System.Drawing.Color.Transparent;
             this.bruh1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 8F, System.Drawing.FontStyle.Bold);
-            this.bruh1.Location = new System.Drawing.Point(180, 157);
+            this.bruh1.Location = new System.Drawing.Point(240, 193);
+            this.bruh1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.bruh1.Name = "bruh1";
-            this.bruh1.Size = new System.Drawing.Size(0, 13);
+            this.bruh1.Size = new System.Drawing.Size(0, 19);
             this.bruh1.TabIndex = 11;
             this.bruh1.Click += new System.EventHandler(this.bruh1_Click);
             // 
             // bruh2
             // 
             this.bruh2.AutoSize = true;
+            this.bruh2.BackColor = System.Drawing.Color.Transparent;
             this.bruh2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 8F, System.Drawing.FontStyle.Bold);
-            this.bruh2.Location = new System.Drawing.Point(250, 158);
+            this.bruh2.Location = new System.Drawing.Point(333, 194);
+            this.bruh2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.bruh2.Name = "bruh2";
-            this.bruh2.Size = new System.Drawing.Size(0, 13);
+            this.bruh2.Size = new System.Drawing.Size(0, 19);
             this.bruh2.TabIndex = 12;
             // 
             // movingPanel
@@ -119,8 +128,9 @@ namespace d20
             this.movingPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.movingPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.movingPanel.Location = new System.Drawing.Point(0, 0);
+            this.movingPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.movingPanel.Name = "movingPanel";
-            this.movingPanel.Size = new System.Drawing.Size(500, 17);
+            this.movingPanel.Size = new System.Drawing.Size(667, 21);
             this.movingPanel.TabIndex = 13;
             this.movingPanel.TabStop = false;
             this.movingPanel.Click += new System.EventHandler(this.movingPanel_Click);
@@ -130,33 +140,39 @@ namespace d20
             // 
             // panel4
             // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
             this.panel4.BackgroundImage = global::d20.Properties.Resources.long_flag;
             this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel4.Location = new System.Drawing.Point(370, -75);
+            this.panel4.Location = new System.Drawing.Point(493, -92);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(130, 364);
+            this.panel4.Size = new System.Drawing.Size(173, 448);
             this.panel4.TabIndex = 9;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
             // panel6
             // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
             this.panel6.BackgroundImage = global::d20.Properties.Resources.border60_2;
             this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel6.Location = new System.Drawing.Point(0, 400);
+            this.panel6.Location = new System.Drawing.Point(0, 492);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(500, 100);
+            this.panel6.Size = new System.Drawing.Size(667, 123);
             this.panel6.TabIndex = 8;
             // 
             // d20Gif
             // 
+            this.d20Gif.BackColor = System.Drawing.Color.Transparent;
             this.d20Gif.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.d20Gif.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.d20Gif.ErrorImage = global::d20.Properties.Resources.ezgif_3_82f1eb50ba87;
             this.d20Gif.Image = global::d20.Properties.Resources._150x150d20;
             this.d20Gif.ImageLocation = "";
-            this.d20Gif.Location = new System.Drawing.Point(157, 60);
+            this.d20Gif.Location = new System.Drawing.Point(209, 74);
+            this.d20Gif.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.d20Gif.Name = "d20Gif";
-            this.d20Gif.Size = new System.Drawing.Size(223, 167);
+            this.d20Gif.Size = new System.Drawing.Size(297, 206);
             this.d20Gif.TabIndex = 2;
             this.d20Gif.TabStop = false;
             this.d20Gif.WaitOnLoad = true;
@@ -164,33 +180,39 @@ namespace d20
             // 
             // buttonPlus1
             // 
+            this.buttonPlus1.BackColor = System.Drawing.Color.Transparent;
             this.buttonPlus1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonPlus1.Location = new System.Drawing.Point(269, 239);
+            this.buttonPlus1.Location = new System.Drawing.Point(359, 294);
+            this.buttonPlus1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonPlus1.Name = "buttonPlus1";
-            this.buttonPlus1.Size = new System.Drawing.Size(50, 50);
+            this.buttonPlus1.Size = new System.Drawing.Size(67, 62);
             this.buttonPlus1.TabIndex = 14;
             this.buttonPlus1.Text = "+1";
-            this.buttonPlus1.UseVisualStyleBackColor = true;
+            this.buttonPlus1.UseVisualStyleBackColor = false;
             this.buttonPlus1.Click += new System.EventHandler(this.buttonPlus1_Click);
             // 
             // buttonMinus1
             // 
+            this.buttonMinus1.BackColor = System.Drawing.Color.Transparent;
             this.buttonMinus1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonMinus1.Location = new System.Drawing.Point(139, 239);
+            this.buttonMinus1.Location = new System.Drawing.Point(185, 294);
+            this.buttonMinus1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonMinus1.Name = "buttonMinus1";
-            this.buttonMinus1.Size = new System.Drawing.Size(50, 50);
+            this.buttonMinus1.Size = new System.Drawing.Size(67, 62);
             this.buttonMinus1.TabIndex = 15;
             this.buttonMinus1.Text = "-1";
-            this.buttonMinus1.UseVisualStyleBackColor = true;
+            this.buttonMinus1.UseVisualStyleBackColor = false;
             this.buttonMinus1.Click += new System.EventHandler(this.buttonMinus1_Click);
             // 
             // mod
             // 
             this.mod.AutoSize = true;
+            this.mod.BackColor = System.Drawing.Color.Transparent;
             this.mod.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.mod.Location = new System.Drawing.Point(223, 267);
+            this.mod.Location = new System.Drawing.Point(297, 329);
+            this.mod.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.mod.Name = "mod";
-            this.mod.Size = new System.Drawing.Size(14, 13);
+            this.mod.Size = new System.Drawing.Size(17, 17);
             this.mod.TabIndex = 16;
             this.mod.Text = "0";
             this.mod.Click += new System.EventHandler(this.mod_Click);
@@ -198,50 +220,59 @@ namespace d20
             // modifierLabel
             // 
             this.modifierLabel.AutoSize = true;
+            this.modifierLabel.BackColor = System.Drawing.Color.Transparent;
             this.modifierLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.modifierLabel.Location = new System.Drawing.Point(205, 240);
+            this.modifierLabel.Location = new System.Drawing.Point(273, 295);
+            this.modifierLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.modifierLabel.Name = "modifierLabel";
-            this.modifierLabel.Size = new System.Drawing.Size(49, 20);
+            this.modifierLabel.Size = new System.Drawing.Size(62, 25);
             this.modifierLabel.TabIndex = 17;
             this.modifierLabel.Text = "MOD";
             // 
             // modifierResetButton
             // 
+            this.modifierResetButton.BackColor = System.Drawing.Color.Transparent;
             this.modifierResetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.modifierResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.modifierResetButton.Location = new System.Drawing.Point(138, 291);
+            this.modifierResetButton.Location = new System.Drawing.Point(184, 358);
+            this.modifierResetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.modifierResetButton.Name = "modifierResetButton";
-            this.modifierResetButton.Size = new System.Drawing.Size(180, 25);
+            this.modifierResetButton.Size = new System.Drawing.Size(240, 31);
             this.modifierResetButton.TabIndex = 18;
             this.modifierResetButton.Text = "RESET MODIFIER";
-            this.modifierResetButton.UseVisualStyleBackColor = true;
+            this.modifierResetButton.UseVisualStyleBackColor = false;
             this.modifierResetButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // modplusroll
             // 
             this.modplusroll.AutoSize = true;
-            this.modplusroll.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.modplusroll.Location = new System.Drawing.Point(213, 88);
+            this.modplusroll.BackColor = System.Drawing.Color.Transparent;
+            this.modplusroll.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.modplusroll.Location = new System.Drawing.Point(284, 108);
+            this.modplusroll.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.modplusroll.Name = "modplusroll";
-            this.modplusroll.Size = new System.Drawing.Size(0, 14);
+            this.modplusroll.Size = new System.Drawing.Size(0, 17);
             this.modplusroll.TabIndex = 19;
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
             this.panel1.BackgroundImage = global::d20.Properties.Resources.long_flag1;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel1.Location = new System.Drawing.Point(-3, -75);
+            this.panel1.Location = new System.Drawing.Point(-4, -92);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(130, 364);
+            this.panel1.Size = new System.Drawing.Size(173, 448);
             this.panel1.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(667, 615);
             this.Controls.Add(this.modplusroll);
             this.Controls.Add(this.modifierResetButton);
             this.Controls.Add(this.modifierLabel);
@@ -249,7 +280,6 @@ namespace d20
             this.Controls.Add(this.buttonMinus1);
             this.Controls.Add(this.buttonPlus1);
             this.Controls.Add(this.bruh1);
-            this.Controls.Add(this.movingPanel);
             this.Controls.Add(this.bruh2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel4);
@@ -258,9 +288,11 @@ namespace d20
             this.Controls.Add(this.lblChangeText);
             this.Controls.Add(this.d20Gif);
             this.Controls.Add(this.rollbutton);
+            this.Controls.Add(this.movingPanel);
             this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
